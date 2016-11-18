3a39977c903c725baf40d558c79c1433fd2dbb9b
shahn3 <shahn3@asbindia.org>
2016-11-18 13:14:31 +0530

    Commit 0.02
    In this commit, I added the background that I want and also the music,
    the music will change as soon as I get a song in mind. Next, I want to
    get a player and start moving him around.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
-function setup() {
+var backgroundImage;
+var mySound;
 
+function preload()
+{
+  mySound = loadSound('assets/Adele - Rumour has it (with lyrics).mp3');
+  backgroundImage = loadImage("assets/codingProject_footballField.jpg");
 }
 
-function draw() {
+function setup() 
+{
+  createCanvas(1000,600);
+  musicVolume();
+}
 
+function draw() 
+{
+  background(backgroundImage);
+}
+
+
+function musicVolume()
+{
+  mySound.setVolume(0.5);
+  mySound.play();
 }
