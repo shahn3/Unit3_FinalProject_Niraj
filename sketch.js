4a3de3ce0005a94e344e314eb3af088ffb4a78bf
shahn3 <hahn3@asbindia.org>
2016-11-30 15:26:37 +0530

    Commit 0.04
    I added a Wide Receiver and I am working on having him where I want to
    and what he should do if he ‘catches’ the ball.

--- sketch.js
+++ sketch.js
@@ -1,35 +1,95 @@
 var backgroundImage;
 var mySound;
 var qbThrowing
+var wrReady;
+var wrSuccess;
+var rand;
 
 function preload()
 {
   mySound = loadSound('assets/Adele - Rumour has it (with lyrics).mp3');
   backgroundImage = loadImage("assets/codingProject_footballField.jpg");
   qbThrowing = loadImage("assets/QB_Throwing.png");
-  
+  qbSad = loadImage("assets/QB_Sad.png")
+  wrReady = loadImage("assets/wr_Ready.png")
+  wrSuccess = loadImage("assets/wr_Success.png")
 }
 
 function setup() 
 {
   createCanvas(1000,600);
-  musicVolume();
+  rand = int(random(3));
 }
+
 
 function draw() 
 {
   background(backgroundImage);
   QuarterBack();
+  Ready();
 }
 
 
 function musicVolume()
 {
-  mySound.setVolume(0.5);
+  mySound.setVolume(1);
   mySound.play();
 }
 
 function QuarterBack()
 {
   image(qbThrowing, 150, 250, 100, 100);
+}
+
+function Ready()
+{
+  print(rand);
+  
+  if (rand == 0)
+  {
+    image(wrReady, 250, 250, 100, 100);
+  }
+  
+  if (rand == 1)
+  {
+  image(wrReady, 350, 350, 100, 100);
+  }
+  
+  if (rand == 2)
+  {
+    image(wrReady, 450, 450, 100, 100);
+  }
+}
+
+function wrCatch()
+{
+  
+}
+
+function keyPressed()
+{
+    
+   if (rand == 0 && keyCode === UP_ARROW)
+   {
+     image(wrSuccess, 250, 100, 100, 100);
+     //and text that you succeeded 
+   }
+   
+   if (rand == 1 && keyCode === DOWN_ARROW)
+   {
+     image(wrSuccess, 150, 300, 100, 100);
+     //and text that you succeeded 
+   }
+   
+   if (rand == 2 && keyCode === RIGHT_ARROW)
+   {
+     image(wrSuccess, 250, 250, 100, 100);
+     //and text that you succeeded 
+   }
+   
+   else
+   {
+     image(qbSad, 150, 250, 100, 100)
+     //and text that you failed 
+   }
 }
